472d46af1550bf0a06ca46a3a4fa000009934fce
AmalShafey <amal.shafey2002@gmail.com>
2025-05-01 22:51:21 +0300

    newchange

--- Innova_Hub-main/lib/home/widget/listorder.dart
+++ Innova_Hub-main/lib/home/widget/listorder.dart
@@ -11,6 +11,99 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  // Map to store ratings per item (using index as key)
+  Map<int, int> ratings = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: CartController.cartItems.length,
+      itemBuilder: (context, index) {
+        final item = CartController.cartItems[index];
+        final currentRating = ratings[index] ?? 0;
+
+        return Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [BoxShadow(color: Colors.grey.shade300, blurRadius: 4)],
+          ),
+          child: Row(
+            children: [
+              const SizedBox(width: 10),
+              Container(
+                height: 90,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      item["HomePictureUrl"],
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "How Find This Product",
+                      style: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 5),
+                    Row(
+                      children: List.generate(5, (starIndex) {
+                        return IconButton(
+                          icon: Icon(
+                            starIndex < currentRating
+                                ? Icons.star
+                                : Icons.star_border_outlined,
+                            color: starIndex < currentRating
+                                ? Colors.amber
+                                : Colors.grey,
+                            size: 30,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              ratings[index] = starIndex + 1;
+                            });
+                          },
+                        );
+                      }),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:innovahub_app/core/Constants/Colors_Constant.dart';
+import 'package:innovahub_app/home/cart_Tap.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -72,3 +165,4 @@ class _MyWidgetState extends State<MyWidget> {
     );
   }
 }
+*/
--- Innova_Hub-main/lib/profiles/Widgets/myorder.dart
+++ Innova_Hub-main/lib/profiles/Widgets/myorder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:innovahub_app/core/Constants/Colors_Constant.dart';
 import 'package:innovahub_app/home/cart_Tap.dart';
 import 'package:innovahub_app/home/widget/listorder.dart';
 
@@ -13,8 +14,66 @@ class myorder extends StatelessWidget {
           "Review",
         ),
       ),
-      body: Column(
-        children: [MyWidget()],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 4),
+        child: Column(
+          children: [
+            MyWidget(),
+            Container(
+              height: 200,
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Constant.whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(14),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 130,
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Constant.whiteColor,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Constant.greyColor4),
+                      ),
+                      child: const Text(
+                        "What Should Other Custom Know !",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Constant.greyColor4,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Constant.mainColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  minimumSize: const Size(220, 50),
+                ),
+                child: const Text('Submit',
+                    style: TextStyle(fontSize: 18, color: Constant.whiteColor)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
